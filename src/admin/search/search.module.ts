ccb499ce010810011de960ad1ed926d9a0e78307
Imesh Munasinghe <imesh5lk@gmail.com>
2026-04-18 19:10:57 +0530

    Import FirebaseModule and register FirebaseAuthGuard

--- src/admin/search/search.module.ts
+++ src/admin/search/search.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AdminSearchController } from './search.controller.js';
 import { SearchModule } from '../../shared/search/search.module.js';
+import { FirebaseModule } from '../../shared/firebase/firebase.module.js';
+import { FirebaseAuthGuard } from '../../auth/firebase-auth.guard.js';
 
 @Module({
-  imports: [SearchModule],
+  imports: [SearchModule, FirebaseModule],
   controllers: [AdminSearchController],
+  providers: [FirebaseAuthGuard],
 })
 export class AdminSearchModule {}
