eca341003abde334ef8bdeef32e26851d8f91ecc
Imesh Munasinghe <imesh5lk@gmail.com>
2026-08-18 01:09:12 +0530

    feat(mail): replace Nodemailer SMTP with Resend HTTP API

--- src/shared/mail/mail.module.ts
+++ src/shared/mail/mail.module.ts
@@ -1,33 +1,8 @@
 import { Module } from '@nestjs/common';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MailService } from './mail.service.js';
 
 @Module({
-  imports: [
-    MailerModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        transport: {
-          host: 'smtp.gmail.com',
-          port: 587,
-          secure: false,
-          requireTLS: true,
-          // @ts-ignore: Force IPv4 as Railway drops IPv6
-          family: 4,
-          auth: {
-            user: config.get<string>('MAIL_USER'),
-            pass: config.get<string>('MAIL_PASS'),
-          },
-        },
-        defaults: {
-          from: config.get<string>('MAIL_FROM'),
-        },
-      }),
-    }),
-  ],
   providers: [MailService],
   exports: [MailService],
 })
-export class MailModule { }
+export class MailModule {}
