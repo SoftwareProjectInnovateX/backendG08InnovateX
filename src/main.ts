efb62b1905e06e4389aa5134143a8b0a1a4a9658
Imesh Munasinghe <imesh5lk@gmail.com>
2026-08-17 22:42:21 +0530

    fix(dns): force IPv4 DNS resolution order first globally

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+import dns from 'node:dns';
+dns.setDefaultResultOrder('ipv4first');
+
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module.js';
